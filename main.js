466b8cdf17e1618fc95b719a2537a13759f556ac
Anthony Stolp <anthonystolp@anthonys-mbp-2.home>
2019-12-11 15:56:00 -0600

    everythings working

--- main.js
+++ main.js
@@ -1,24 +1,12 @@
-function submitForm() {
-    $('form').on('click', function (e){
-        e.preventDefault();
-        const userValue = $('#inputText').val();
-        acquireData(userValue);
-        $('.data').empty();
-    })
-    //this function is going to listen form an event on the form
-    //then is needs to grab the users input and prevent default submission
-    //then pass in displayResults function to help display the results lol
-}
-
-function acquireData(userName) {
-    fetch (`https://api.github.com/users/${userName}/repos`)
+function acquireData(userValue) {
+    fetch (`https://api.github.com/users/${userValue}/repos`)
     .then(response => {
         if(response.ok) {
             return response.json();
         }
         throw new Error(response.statusText);
     })
-    .then(responseJson => displayResults(responseJson, maxResults))
+    .then(responseJson => displayResults(responseJson))
     .catch(error => {
         $('.message').text(`Something happened behind the scenes: ${error.message}`);
     });
@@ -27,9 +15,33 @@ function acquireData(userName) {
     //this function will use parameters 
 }
 
-function displayResults() {
+function displayResults(responseJson) {
+    console.log(responseJson);
+    const userValue = $('#userVal').val();
+    let userRepo = responseJson;
+    $('.data').append(`<h2>${userValue}</h2`)
+    $('.data').append(`<ul class='user-results'></ul>`) 
+        for (let i = 0; i < userRepo.length; i++){
+            console.log(userRepo[i].name);
+            console.log(userRepo[i].html_url);
+            $('.data').append(`<li>
+            <p><a href="${userRepo[i].html_url}">${userRepo[i].name}</a></p>
+            </li>`)
+        }
+    }
+
     //this function is going to use the data from the responseJSON
     //jquery will be used to traverse the dom to display the data
+function submitForm() {
+    $('#inputText').on('click', function (e){
+        e.preventDefault();
+        const userValue = $('#userVal').val();
+        acquireData(userValue);
+        $('.data').empty();
+    })
+    //this function is going to listen form an event on the form
+    //then is needs to grab the users input and prevent default submission
+    //then pass in displayResults function to help display the results lol
 }
 
 submitForm();
